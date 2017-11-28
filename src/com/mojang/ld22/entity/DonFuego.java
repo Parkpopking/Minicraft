8f488b46cfdc673ba2f37e9ecd0d5d52e639d71d
Parkpopking <34077186+Parkpopking@users.noreply.github.com>
2017-11-28 18:57:02 -0400

    Update DonFuego.java

--- src/com/mojang/ld22/entity/DonFuego.java
+++ src/com/mojang/ld22/entity/DonFuego.java
@@ -1,4 +1,10 @@
+package com.mojang.ld22.entity;
 
+import com.mojang.ld22.gfx.Color;
+import com.mojang.ld22.gfx.Screen;
+import com.mojang.ld22.item.ResourceItem;
+import com.mojang.ld22.item.resource.Resource;
+import com.mojang.ld22.sound.Sound;
 
 public class DonFuego extends Mob {
 
