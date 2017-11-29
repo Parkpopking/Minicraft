2635f4543636e0241422a799a2c2636b28ff3764
Parkpopking <34077186+Parkpopking@users.noreply.github.com>
2017-11-28 20:58:02 -0400

    Update sky.java

--- src/com/mojang/ld22/level/levelgen/biomegen/sky.java
+++ src/com/mojang/ld22/level/levelgen/biomegen/sky.java
@@ -1,8 +1,66 @@
 //This just covers formation for the sky level
 
-
+import com.mojang.ld22.level.*;
 
 
 public class sky extends biome {
+  	private static byte[][] createSkyMap(int w, int h) {
+		LevelGen noise1 = new LevelGen(w, h, 8);
+		LevelGen noise2 = new LevelGen(w, h, 8);
 
+		byte[] map = new byte[w * h];
+		byte[] data = new byte[w * h];
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				int i = x + y * w;
+
+				double val = Math.abs(noise1.values[i] - noise2.values[i]) * 3 - 2;
+
+				double xd = x / (w - 1.0) * 2 - 1;
+				double yd = y / (h - 1.0) * 2 - 1;
+				if (xd < 0) xd = -xd;
+				if (yd < 0) yd = -yd;
+				double dist = xd >= yd ? xd : yd;
+				dist = dist * dist * dist * dist;
+				dist = dist * dist * dist * dist;
+				val = -val * 1 - 2.2;
+				val = val + 1 - dist * 20;
+
+				if (val < -0.25) {
+					map[i] = Tile.infiniteFall.id;
+				} else {
+					map[i] = Tile.cloud.id;
+				}
+			}
+		}
+
+		stairsLoop: for (int i = 0; i < w * h / 50; i++) {
+			int x = random.nextInt(w - 2) + 1;
+			int y = random.nextInt(h - 2) + 1;
+
+			for (int yy = y - 1; yy <= y + 1; yy++)
+				for (int xx = x - 1; xx <= x + 1; xx++) {
+					if (map[xx + yy * w] != Tile.cloud.id) continue stairsLoop;
+				}
+
+			map[x + y * w] = Tile.cloudCactus.id;
+		}
+
+		int count = 0;
+		stairsLoop: for (int i = 0; i < w * h; i++) {
+			int x = random.nextInt(w - 2) + 1;
+			int y = random.nextInt(h - 2) + 1;
+
+			for (int yy = y - 1; yy <= y + 1; yy++)
+				for (int xx = x - 1; xx <= x + 1; xx++) {
+					if (map[xx + yy * w] != Tile.cloud.id) continue stairsLoop;
+				}
+
+			map[x + y * w] = Tile.stairsDown.id;
+			count++;
+			if (count == 2) break;
+		}
+
+		return new byte[][] { map, data };
+	}
 }
