0b69037455dea686ee7be8d3e84481a29fcd4565
Parkpopking <34077186+Parkpopking@users.noreply.github.com>
2017-11-29 18:05:28 -0400

    Update Dwarf.java

--- src/com/mojang/ld22/entity/Dwarf.java
+++ src/com/mojang/ld22/entity/Dwarf.java
@@ -1,5 +1,13 @@
 import com.mojang.ld22.entity.*;
 
-public class Dwarf extends Mob {
+import com.mojang.ld22.gfx.Color;
+import com.mojang.ld22.gfx.Screen;
+import com.mojang.ld22.item.ResourceItem;
+import com.mojang.ld22.item.resource.Resource;
 
+public class Dwarf extends Mob {
+    float xa;
+    float ya
+			
+  
 }
